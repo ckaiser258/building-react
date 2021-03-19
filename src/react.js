3bec5c6027dd655ba4eb4d4e88e1b45d5d1ba552
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-19 11:41:58 -0600

    Create performUnitOfWork() and createDom() functions.

--- src/react.js
+++ src/react.js
@@ -32,7 +32,43 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {}
+function performUnitOfWork(fiber) {
+  //Add DOM node
+  if (!fiber.dom) fiber.dom = createDom(fiber);
+
+  if (fiber.parent) fiber.parent.appendChild(fiber.dom);
+
+  //Create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    //Add it to the fiber tree as either a child or sibling depending on if it's the first child or not
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+
+  //Return the next unit of work
+  if (fiber.child) return fiber.child;
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+    nextFiber = nextFiber.parent;
+  }
+}
 
 const Didact = {
   createElement,
@@ -48,7 +84,7 @@ const element = (
   </div>
 );
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
     element.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
@@ -61,9 +97,19 @@ function render(element, container) {
       dom[name] = element.props[name];
     });
 
-  element.props.children.forEach((child) => render(child, dom));
+  return dom;
+}
 
-  container.appendChild(dom);
+let nextUnitOfWork = null;
+
+function render(element, container) {
+  //set next unit of work
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: element,
+    },
+  };
 }
 
 Didact.render(element, container);
